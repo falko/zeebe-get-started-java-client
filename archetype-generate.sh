41a6ca32055fc027ec03612d7bece4c218f42708
Falko Menge <falko.menge@camunda.com>
2020-09-18 21:46:16 +0200

    Improve archetype generation

--- archetype-generate.sh
+++ archetype-generate.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 mvn clean archetype:create-from-project -Darchetype.properties=archetype.properties
-archetype=target/generated-sources/archetype
-mkdir --parents "${archetype}src/it/projects/basic"
-echo 'package' > "${archetype}src/it/projects/basic/goal.txt"
-cp archetype-test.properties "${archetype}src/it/projects/basic/archetype.properties"
-mvn clean install
+archetype=target/generated-sources/archetype/
+mkdir --parents "${archetype}src/test/resources/projects/basic/"
+echo 'package' > "${archetype}src/test/resources/projects/basic/goal.txt"
+cp archetype-test.properties "${archetype}src/test/resources/projects/basic/archetype.properties"
+cd ${archetype}
+sed -i 's/package ${groupId};/package ${package};/' src/main/resources/archetype-resources/src/main/java/Application.java
+sed -i 's/${groupId}.client/io.zeebe.client/' src/main/resources/archetype-resources/src/main/java/Application.java
+mvn eclipse:eclipse integration-test # FIXME doesn't generate m2e project, better copy .project, .classpath & .settings from m2e project
+cd target/test-classes/projects/basic/project/zeebe-demo-test/
+mvn eclipse:eclipse # FIXME doesn't generate m2e project, better copy .project, .classpath & .settings from m2e project
+cd -
+mvn install
+# either manually add the archetype into the Eclipse archetype catalogue using its Maven coordinates
+# or run mvn clean install on the archetype project in Eclipse (integration test must probably be skipped)
--- archetype-todos.md
+++ archetype-todos.md
@@ -1,5 +1,9 @@
-- [ ] correct package declation, currently `io.zeebe.demo` instead of e.g. `io.zeebe.demo.zeebe_starter_2019_08_12`
-- [ ] correct  zeebe client imports, currently `io.zeebe.demo.client.ZeebeClient` instead of `io.zeebe.client.ZeebeClient`
-- [ ] version.zeebe property ins archetype's pom.xml and test/archetype.properties (XML Starlet?)
+- [ ] correct package declaration, currently `io.zeebe.demo` instead of e.g. `io.zeebe.demo.zeebe_starter_2019_08_12`
+- [ ] version.zeebe property in archetype's pom.xml and test/archetype.properties (XML Starlet?)
 - [ ] derive version.zeebe from source project using XML Starlet
 - [ ] change version number to -SNAPSHOT
+- [ ] adjust README.md
+  - [ ] remove references to getting started tutorial
+  - [ ] remove license
+  - [ ] remove code of conduct
+- [ ] add .gitignore
